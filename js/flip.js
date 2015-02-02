ece51e094f8515a64378f7c2be57a75d18c561df
Agustin Palmieri <aguja13@hotmail.com>
2015-02-02 13:54:58 -0300

    Definidos modelos y funcionamiento de escaparate

--- js/flip.js
+++ js/flip.js
@@ -4,12 +4,30 @@
 	$rem4     = $('.rem-4'),
 	$rem5     = $('.rem-5'),
 	$rem6     = $('.rem-6'),
+	$rem7     = $('.rem-7'),
+	$rem8     = $('.rem-8'),
+	$rem9     = $('.rem-9'),
+	$rem10     = $('.rem-10'),
+	$rem11     = $('.rem-11'),
+	$rem12    = $('.rem-12'),
+	$rem13    = $('.rem-13'),
+	$rem14    = $('.rem-14'),
+	$rem15    = $('.rem-15'),
 	$min1     = $('#fig-1'),
 	$min2     = $('#fig-2'),
 	$min3     = $('#fig-3'),
 	$min4     = $('#fig-4'),	
 	$min5     = $('#fig-5'),	
 	$min6     = $('#fig-6'),
+	$min7     = $('#fig-7'),
+	$min8     = $('#fig-8'),
+	$min9     = $('#fig-9'),
+	$min10     = $('#fig-10'),
+	$min11     = $('#fig-11'),
+	$min12     = $('#fig-12'),
+	$min13     = $('#fig-13'),
+	$min14     = $('#fig-14'),
+	$min15     = $('#fig-15'),
 	$mod1     = $('.modelo1'),
 	$mod2     = $('.modelo1'),	
 	$mod3     = $('.modelo1'),
@@ -62,6 +80,15 @@ function apareceRemera1() {
 		$rem4.css('display','none' );
 		$rem5.css('display','none' );
 		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
 		$mod1.slideDown(500);		
 };
 function apareceRemera2() {
@@ -70,7 +97,17 @@ function apareceRemera2() {
 		$rem3.css('display','none' );
 		$rem4.css('display','none' );
 		$rem5.css('display','none' );
-		$rem6.css('display','none' );	
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );	
+		$rem15.css('display','none' );
+		
 		// $rem2.fadeIn();
 		$mod2.slideDown(500);		
 	
@@ -80,7 +117,16 @@ function apareceRemera3() {
 		$rem2.css('display','none' );
 		$rem4.css('display','none' );
 		$rem5.css('display','none' );
-		$rem6.css('display','none' );		
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
 		$rem3.slideDown(500);
 $mod3.slideDown(500);		
 };
@@ -90,6 +136,15 @@ function apareceRemera4() {
 		$rem3.css('display','none' );
 		$rem5.css('display','none' );
 		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
 		$rem4.slideDown(500);
 };
 function apareceRemera5() {
@@ -98,6 +153,15 @@ function apareceRemera5() {
 		$rem3.css('display','none' );
 		$rem4.css('display','none' );
 		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
 		$rem5.slideDown(500);		
 };
 function apareceRemera6() {
@@ -106,8 +170,161 @@ function apareceRemera6() {
 		$rem3.css('display','none' );
 		$rem4.css('display','none' );
 		$rem5.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
 		$rem6.slideDown(500);
-$apareceRemera6=true;		
+// $apareceRemera6=true;		
+};
+function apareceRemera7() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );		
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem7.slideDown(500);		
+};
+function apareceRemera8() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );		
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem8.slideDown(500);		
+};
+function apareceRemera9() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );		
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem9.slideDown(500);		
+};
+function apareceRemera10() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );		
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem10.slideDown(500);		
+};
+function apareceRemera11() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );		
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem11.slideDown(500);		
+};
+function apareceRemera12() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );		
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem12.slideDown(500);		
+};
+function apareceRemera13() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );		
+		$rem14.css('display','none' );
+		$rem15.css('display','none' );
+		$rem13.slideDown(500);		
+};
+function apareceRemera14() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem15.css('display','none' );
+		$rem14.slideDown(500);		
+};
+function apareceRemera15() {
+		$rem1.css('display','none' );
+		$rem2.css('display','none' );
+		$rem3.css('display','none' );
+		$rem4.css('display','none' );
+		$rem6.css('display','none' );
+		$rem7.css('display','none' );
+		$rem8.css('display','none' );
+		$rem9.css('display','none' );
+		$rem10.css('display','none' );
+		$rem11.css('display','none' );
+		$rem12.css('display','none' );
+		$rem13.css('display','none' );
+		$rem14.css('display','none' );
+		$rem15.slideDown(500);		
 };
 function apareceS() {
 		// $rem1.css('display','none' );
@@ -153,6 +370,15 @@ $min3.click( apareceRemera3 );
 $min4.click( apareceRemera4 );
 $min5.click( apareceRemera5 );
 $min6.click( apareceRemera6 );
+$min7.click( apareceRemera7 );
+$min8.click( apareceRemera8 );
+$min9.click( apareceRemera9 );
+$min10.click( apareceRemera10 );
+$min11.click( apareceRemera11);
+$min12.click( apareceRemera12);
+$min13.click( apareceRemera13);
+$min14.click( apareceRemera14);
+$min15.click( apareceRemera15);
 $s.click( apareceS );
 $m.click( apareceM );
 $l.click( apareceL );
