d3f25f1ad8828e4c9845582ed6fc4f106b8cecb7
Agustin Palmieri <aguja13@hotmail.com>
2015-02-03 21:46:23 -0300

    lista página contacto y pago

---
+++ contacto.php
@@ -0,0 +1,25 @@
+﻿<?php
+$nombre = $_POST['nombre'];
+$calle = $_POST['calle'];
+$piso = $_POST['piso'];
+$postal = $_POST['postal'];
+$localidad = $_POST['localidad'];
+$provincia = $_POST['provincia'];
+$email = $_POST['email'];
+
+$para = 'aguja13@hotmail.com';
+$titulo = 'Nuevo pedido de remeras';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Calle:\n $calle\n Piso:\n $piso\n Código Postal:\n $postal\n Localidad:\n $localidad\n Provincia:\n $provincia";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.'); 
+window.location.href = 'http://revolution.aguja2013.com/contacto.html';
+</script>";
+} else {
+echo 'Falló el envio';
+}
+}
+?>
